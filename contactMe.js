56192353d2fbd50ae867dd480e63ccdde3ab01e2
Anhad Sodhi <anhadsodhi2005@gmail.com>
2024-02-04 14:47:31 -0800

    Finished??

--- contactMe.js
+++ contactMe.js
@@ -13,3 +13,30 @@ function goToMyEmail() {
 function goToMyPhone() {
     window.open('tel:7788396995');
 }
+
+function copyButtons() {
+    document.getElementById("copyEmail").addEventListener("click", function(e) {
+        let text = document.getElementById("email").innerText;
+        navigator.clipboard.writeText(text);
+        document.getElementById("copyEmail").innerText = "done";
+    })
+
+    document.getElementById("copyPhone").addEventListener("click", function(e) {
+        let text = document.getElementById("phone").innerText;
+        navigator.clipboard.writeText(text);
+        document.getElementById("copyPhone").innerText = "done";
+    })
+
+    document.getElementById("copyLinkedIn").addEventListener("click", function(e) {
+        let text = document.getElementById("linkedIn").innerText;
+        navigator.clipboard.writeText(text);
+        document.getElementById("copyLinkedIn").innerText = "done";
+    })
+
+    document.getElementById("copyGitHub").addEventListener("click", function(e) {
+        let text = document.getElementById("GitHub").innerText;
+        navigator.clipboard.writeText(text);
+        document.getElementById("copyGitHub").innerText = "done";
+    })
+}
+copyButtons();
